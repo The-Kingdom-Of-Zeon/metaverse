623a5f31f97b119b9f83f8e7e2f6feb8c226493e
mindfulme <alice.gogolauri@gmail.com>
2022-03-04 01:08:46 +0200

    in

--- forum/js/foroagency/coloredusername/index.js
+++
@@ -1,2 +0,0 @@
-function usernameColorTagList(){$('body').on('DOMSubtreeModified',function(){$('.autoCompleteList li').each(function(){var color=$(this).find('.ColorUsernameAutoCompleteList').attr('data-color');$(this).attr('style','cursor: pointer; color: '+color);});});}
-$(document).ready(function(){usernameColorTagList();});
